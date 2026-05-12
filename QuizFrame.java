6a913faa6d0e58fff0d0d5eb9bf0abc0728b857a
DEEPIKA-web20 <deepikasakthivel20@gmail.com>
2026-05-12 19:30:50 +0530

    Create QuizFrame.java

---
+++ QuizFrame.java
@@ -0,0 +1,18 @@
+public class QuestionData {
+
+    String questions[][] = {
+            {"Which language is platform independent?", "C", "C++", "Java", "Python"},
+            {"Which company developed Java?", "Google", "Microsoft", "Sun Microsystems", "Apple"},
+            {"Which keyword is used for inheritance?", "implement", "extends", "import", "package"},
+            {"Which method starts a Java program?", "run()", "main()", "start()", "init()"},
+            {"Java is a ?", "Programming Language", "Operating System", "Database", "Browser"}
+    };
+
+    String answers[] = {
+            "Java",
+            "Sun Microsystems",
+            "extends",
+            "main()",
+            "Programming Language"
+    };
+}
